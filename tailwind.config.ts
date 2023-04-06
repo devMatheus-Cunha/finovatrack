b1159347c2f7434b15dbdfe087744c99721ae765
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-06 09:47:35 +0100

    refactor: strutucre of the project

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // ou 'media' ou 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
