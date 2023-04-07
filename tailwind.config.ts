a483134b03530af392896e206e1ce21b81a83834
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-07 18:54:20 +0100

    feat: config css and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 module.exports = {
   mode: "jit",
+  content: [
+    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // ou 'media' ou 'class'
   theme: {
@@ -8,5 +12,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
