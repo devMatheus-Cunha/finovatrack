a32c3e5969d2d690012305825ada252d814227d0
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-08 18:54:53 +0100

    feat: config _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import 'flowbite/dist/flowbite.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
