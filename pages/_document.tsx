c03aff1a3d0bd9370baf1a3b0558d3305de8e0e4
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-08 18:55:03 +0100

    feat: config _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
+import { Sidebar } from 'flowbite-react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import SideBar from '@/templates/sideBar'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <div className='bg-gray-900'>
+          <SideBar>
+            <Main />
+          </SideBar>
+          <NextScript />
+        </div>
       </body>
     </Html>
   )
