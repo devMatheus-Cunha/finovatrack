c4f054a7240a8874c1067cdbed46c332a00ce814
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-09 21:38:16 +0100

    feat: config them dark

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,30 @@ module.exports = {
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // ou 'media' ou 'class'
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1F2937",
+        secondary: "#3E4C59",
+        accent: "#6D28D9",
+        danger: "#EF4444",
+        success: "#22C55E",
+        warning: "#F59E0B",
+        info: "#3B82F6",
+        gray: {
+          100: "#F3F4F6",
+          200: "#E5E7EB",
+          300: "#D1D5DB",
+          400: "#9CA3AF",
+          500: "#6B7280",
+          600: "#4B5563",
+          700: "#374151",
+          800: "#1F2937",
+          900: "#111827",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
