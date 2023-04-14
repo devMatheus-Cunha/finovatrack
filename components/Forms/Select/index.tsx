14f70765f70fe38bbbff6e96b97022f3636be371
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-14 23:27:22 +0100

    feat: create component input

---
+++ components/Forms/Select/index.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react';
+import { FieldError, FieldValues, Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
+
+interface SelectFieldProps<T extends FieldValues> {
+ label: string;
+ name: Path<T>;
+ rules?: RegisterOptions<T>;
+ options: {
+  label: string;
+  value: string;
+ }[];
+ register: UseFormRegister<T>;
+ errors?: ReactNode;
+}
+
+export default function Select<T extends FieldValues>({ label, name, options, register, errors, rules }: SelectFieldProps<T>) {
+ return (
+  <div className="mb-4">
+   <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+    {rules?.required ? `${label} *` : label}
+   </label>
+   <select
+    id={name}
+    {...register(name, rules)}
+    className="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+     "
+   >
+    <option value="" disabled selected>
+    Escolha opção
+    </option>
+    {options.map((option) => (
+     <option key={option.value} value={option.value}>
+      {option.label}
+     </option>
+    ))}
+   </select>
+   {
+    errors && (
+     <>
+      {errors}
+     </>
+    )
+   }
+  </div>
+ );
+}
