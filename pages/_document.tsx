e26e8df610611d01bfd6b9054671cab451f81567
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-15 22:10:55 +0100

    fix: remove sideBar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import SideBar from '@/templates/SideBar'
 
 export default function Document() {
+
 
   return (
     <Html lang="en">
       <Head />
       <body>
-        <SideBar>
-          <Main />
-        </SideBar>
+        <Main />
         <NextScript />
       </body>
     </Html>
