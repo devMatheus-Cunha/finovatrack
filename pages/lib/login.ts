c49b995641c81fcbdbf93ebe51456ab6afcb44bc
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-15 22:11:36 +0100

    feat: add page and requests to login

---
+++ pages/lib/login.ts
@@ -0,0 +1,63 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "@firebase/auth";
+import { auth } from "./firebase";
+
+export const createUserWithEmail = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loginWithEmail = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: any;
+}) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const checkAuthState = async () => {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        user.getIdToken().then(resolve).catch(reject);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+
+export const logout = async () => {
+  return signOut(auth);
+};
