d99186fa39ed567e5fc9e289e9b1bd57e46095cc
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-16 22:14:10 +0100

    feat: create hook to upadted entry

---
+++ pages/control/parts/hooks/useUpadtedEntry/index.ts
@@ -0,0 +1,38 @@
+import { db } from "@/pages/lib/firebase";
+import { doc, updateDoc } from "@firebase/firestore";
+import { useMutation } from "react-query";
+import { useFetchEntrysData } from "..";
+interface IData {
+  value: number;
+  id?: string;
+}
+const useUpadtedEntry = () => {
+  const { refetchEntrysData } = useFetchEntrysData();
+
+  const fetchUpadtedExpense = async ({
+    id,
+    data,
+  }: {
+    id?: string;
+    data: Record<string, any>;
+  }) => {
+    const docRef = doc(db, "entrys", id || "");
+    try {
+      await updateDoc(docRef, data);
+    } catch (error) {
+      console.log();
+    }
+  };
+
+  const { mutate: upadtedEntry } = useMutation(fetchUpadtedExpense, {
+    onSuccess: () => {
+      refetchEntrysData();
+    },
+  });
+
+  return {
+    upadtedEntry,
+  };
+};
+
+export default useUpadtedEntry;
