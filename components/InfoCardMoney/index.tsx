f681894cd24326f60140bb3e5d6fa8c33a4db97c
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-16 23:58:10 +0100

    Update index.tsx

--- components/InfoCardMoney/index.tsx
+++ components/InfoCardMoney/index.tsx
@@ -8,7 +8,7 @@ interface InfoCardProps {
 
 const InfoCardMoney = ({ infoData, title, contentAction }: InfoCardProps) => {
  return (
-  <div className="w-[25%] h-[10vh] focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center  px-4 py-2.5 dark:bg-gray-700  dark:focus:ring-gray-700">
+  <div className="w-[25%] h-[11vh] focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center  px-4 py-2.5 dark:bg-gray-700  dark:focus:ring-gray-700">
    <div className="text-left">
     <div className="mb-1 text-xs">{title}</div>
     <div className="-mt-1 font-sans text-2xl font-semibold">{infoData}</div>
