173dd7ef4885e6d745384c24fb95a8678c8702c2
Matheus Cunha <devmatheusgr@gmail.com>
2023-04-26 23:35:10 +0100

    feat: add toas and validate to fetchQuatationRateFromAPI

--- pages/control/parts/hooks/useFetchQuatationEur/index.ts
+++ pages/control/parts/hooks/useFetchQuatationEur/index.ts
@@ -1,6 +1,8 @@
 import { db } from "@/pages/lib/firebase";
 import { doc, getDoc, updateDoc } from "@firebase/firestore";
 import { useMutation, useQuery }from '@tanstack/react-query';
+import React from "react";
+import { toast } from "react-toastify";
 
 type CurrentQuotation = {
   current_quotation: number;
@@ -17,6 +19,8 @@ export const convertEurosToReais = (quatationEur = 0, valueEur = 0) => {
 };
 
 const useFetchQuatationEur = (amount: string) => {
+  const toastId: any = React.useRef(null);
+
   const upadtedQuotation = async (data: Record<string, any>) => {
     try {
       const myCollection = doc(db, "quotation", "last_quotation_data");
@@ -27,17 +31,21 @@ const useFetchQuatationEur = (amount: string) => {
     }
   };
 
-  const fetchQuatationRateFromAPI = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append("apikey", process.env.NEXT_PUBLIC_API_KEY_EXCHANGE || "");
+const fetchQuatationRateFromAPI = async (value: string) => {
+  const myHeaders = new Headers();
+  myHeaders.append("apikey", process.env.NEXT_PUBLIC_API_KEY_EXCHANGE || "");
 
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-    };
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  if (toastId.current === null) {
+    toastId.current = toast('Atualizando Cotação...', { autoClose: false });
+  }
 
     const response = await fetch(
-      `https://api.apilayer.com/exchangerates_data/convert?to=brl&from=eur&amount=${amount}`,
+      `https://api.apilayer.com/exchangerates_data/convert?to=brl&from=eur&amount=${value}`,
       requestOptions
     );
 
@@ -45,13 +53,14 @@ const useFetchQuatationEur = (amount: string) => {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
+
     return {
-      result_calculation: convertEurosToReais(data.info.rate, Number(amount)),
+      result_calculation: convertEurosToReais(data.info.rate, Number(value)),
       current_quotation: data.info.rate,
       date: data?.date,
       status: data.success,
-    };
-  };
+    }
+};
 
   const fetchLastQuotationData = async () => {
     const docRef = doc(db, "quotation", "last_quotation_data");
@@ -68,20 +77,34 @@ const useFetchQuatationEur = (amount: string) => {
   const { data: lastQuatationData, refetch: refetchLastQuotationData } =
     useQuery(["last_quotation_data"], async () => await fetchLastQuotationData());
 
-  const { mutate: addlastQuotation } = useMutation(upadtedQuotation, {
+  const { mutate: addLastQuotation } = useMutation(upadtedQuotation, {
     onSuccess: async () => await refetchLastQuotationData(),
   });
 
-  const { refetch: refetchQuationData } = useQuery({
+  const { refetch: refetchQuationData, fetchStatus } = useQuery({
     queryKey: ["quatation_data"],
-    queryFn: fetchQuatationRateFromAPI,
+    queryFn: () => fetchQuatationRateFromAPI(amount),
     enabled: false,
     cacheTime: 0,
     onSuccess: (res) => {
-      addlastQuotation(res);
+      addLastQuotation(res);
+       toast.update(toastId.current, {
+        type: 'success',
+        render: 'Sucesso ao Atualizar Cotação',
+        className: 'rotateY animated',
+        autoClose: 5000,
+       })
+      toastId.current = null;
+    },
+    onError: () => {
+      toast.update(toastId.current, {
+        type: 'error',
+        render: 'Erro ao Atualizar Cotação',
+        autoClose: 5000,
+      });
+      toastId.current = null;
     },
   });
-
   return { lastQuatationData, refetchQuationData };
 };
 
