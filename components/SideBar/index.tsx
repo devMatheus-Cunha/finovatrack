24ae587c1b9bd6de5862543aeb1dcfa78b6f051d
Matheus Cunha <devmatheusgr@gmail.com>
2023-05-04 11:24:03 +0100

    refactor: sideBar

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,5 +1,5 @@
 import router, { useRouter } from "next/router";
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout } from "@/pages/lib/login";
 import { Archive, ChartPie, ClipboardText, Eye, EyeSlash, SignOut } from "@phosphor-icons/react";
 import useIsVisibilityDatas from "../../hooks/useIsVisibilityDatas"
@@ -8,6 +8,17 @@ import React from "react";
 export default function SideBar({ children }: any) {
  const { isVisibilityData, handleToggleVisibilityData } = useIsVisibilityDatas()
  const routeName = useRouter().pathname.replace('/', '')
+ const queryClient = useQueryClient()
+ const { mutate: onLogout } = useMutation(logout, {
+  onSuccess: () => {
+   queryClient.setQueryData(["auth_state"], {
+    id: undefined,
+    expirationTimeToken: undefined,
+    token: undefined,
+   });
+   router.push('/');
+  },
+ });
 
  const sidebarItems = [
   {
@@ -34,17 +45,19 @@ export default function SideBar({ children }: any) {
    icon: <ChartPie size={24} color={routeName === "statistics" ? "#06b6d4" : "#eee2e2"} />,
    action: () => router.push("/statistics"),
   },
+  {
+   id: "logout",
+   label: "Sair",
+   icon: <SignOut size={24} />,
+   action: () => onLogout(),
+  },
  ];
 
- const { mutate: onLogout } = useMutation(logout, {
-  onSuccess: () => {
-   router.push('/');
-  },
- });
+
  return (
   <div className="h-[100vh] flex">
    <aside id="default-sidebar" className="w-auto h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div className="h-full flex flex-col justify-between	px-3 py-4 dark:bg-gray-800">
+    <div className="h-full flex flex-col	px-3 py-4 dark:bg-gray-800">
      <div className="flex flex-col gap-6">
       {
        sidebarItems.map((item) => (
@@ -60,13 +73,13 @@ export default function SideBar({ children }: any) {
        ))
       }
      </div>
-     <button
+     {/* <button
       onClick={() => onLogout()}
       type="button"
       className="focus:outline-none font-medium rounded-lg text-md dark:tansparent dark:focus:ring-gray-600 dark:border-gray-600 w-[100%] flex items-center justify-center"
      >
       <SignOut size={24} color="#eee2e2" />
-     </button>
+     </button> */}
     </div>
    </aside >
    <div className='flex-auto'>
