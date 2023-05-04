ebb6262273fc30ff8ee11f933c9fbec39f6d51b1
Matheus Cunha <devmatheusgr@gmail.com>
2023-05-04 11:23:04 +0100

    feat: create flux to createAccount

--- pages/lib/login.ts
+++ pages/lib/login.ts
@@ -3,6 +3,7 @@ import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
 } from "@firebase/auth";
 import { auth } from "./firebase";
 
@@ -40,23 +41,70 @@ export const loginWithEmail = async ({
       password
     );
     const user = userCredential.user;
+    console.log(user)
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+export const createAccountUser = async ({
+  email,
+  password,
+  name,
+  type_account
+}: {
+  email: string;
+  password: any;
+  name: string
+  type_account: string
+}) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    const user = userCredential.user;
+    if (user) {
+       await updateProfile(user, {
+         displayName: name || "",
+         photoURL: type_account
+    })
+    }
     return user;
   } catch (error) {
     throw error;
   }
 };
 
-export const checkAuthState = async () => {
+ export const checkAuthState = (): Promise<{
+  id: string,
+  expirationTimeToken: string,
+  token: string,
+  email: string,
+   name: string
+  typeAccount: string
+} | null> => {
   return new Promise((resolve, reject) => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      unsubscribe();
       if (user) {
-        user.getIdToken().then(resolve).catch(reject);
+        const idTokenResult = await user.getIdTokenResult(true);
+        resolve({
+          id: user.uid,
+          expirationTimeToken: idTokenResult.expirationTime,
+          token: idTokenResult.token,
+          email: user.email ?? "",
+          name: user.displayName ?? "",
+          typeAccount: user.photoURL ?? "",
+        });
       } else {
         resolve(null);
       }
-    });
+    }, reject);
   });
 };
+
 
 export const logout = async () => {
   return signOut(auth);
