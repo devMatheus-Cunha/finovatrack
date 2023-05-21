c3d53f190da0918749eca02d9d90058ef2167914
Matheus Cunha <devmatheusgr@gmail.com>
2023-05-21 20:36:39 +0100

    refactor: inputs

--- src/components/Forms/Input/index.tsx
+++ src/components/Forms/Input/index.tsx
@@ -5,7 +5,7 @@
 'use client';
 
 import {
-  FieldValues, Path, RegisterOptions, UseFormRegister,
+  FieldValues, Path, UseFormRegister,
 } from 'react-hook-form';
 import React, { InputHTMLAttributes, ReactNode } from 'react';
 
@@ -13,22 +13,22 @@ interface InputProps<T extends FieldValues> extends InputHTMLAttributes<HTMLInpu
   label: string;
   name: Path<T>;
   register: UseFormRegister<T>;
-  rules?: RegisterOptions<T>;
+  required?: boolean;
   errors?: ReactNode;
 }
 
 export default function Input<T extends FieldValues>({
-  label, name, register, rules, errors, disabled, ...rest
+  label, name, register, required, errors, disabled, ...rest
 }: InputProps<T>) {
   return (
     <div className="w-full">
       <label htmlFor={name} className="block mb-2 text-sm font-medium text-white">
-        {rules?.required ? `${label} *` : label}
+        {required ? `${label} *` : label}
       </label>
       <input
         id={name as string}
         disabled={disabled}
-        {...register(name, rules)}
+        {...register(name)}
         className={disabled
           ? 'border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-gray-300 cursor-not-allowed'
           : 'border text-sm rounded-lg block w-full p-2.5 bg-gray-800 border-gray-700 placeholder-gray-400 text-white'}
