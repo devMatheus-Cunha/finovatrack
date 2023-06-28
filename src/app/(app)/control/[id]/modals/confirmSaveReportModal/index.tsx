e7f5b2bdb6ee5cc97860041be11396809d2217d8
Matheus Cunha <devmatheusgr@gmail.com>
2023-06-28 21:35:51 +0100

    add option to save report to a date

--- src/app/(app)/control/[id]/modals/confirmSaveReportModal/index.tsx
+++ src/app/(app)/control/[id]/modals/confirmSaveReportModal/index.tsx
@@ -2,14 +2,15 @@
 
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { FolderPlus } from '@phosphor-icons/react'
 import { ExpenseData } from '@/hooks/expenses/useFetchExpensesData'
 import { Button } from '@/components'
+import DatePicker from 'react-datepicker'
 
 interface IConfirmSaveReportModal {
-  onSubmit: (data: ExpenseData[]) => void
+  onSubmit: (data: any) => void
   onCancel: () => void
   initialData: ExpenseData[]
 }
@@ -19,15 +20,36 @@ function ConfirmSaveReportModal({
   onCancel,
   initialData,
 }: IConfirmSaveReportModal) {
+  const [selectedPeriod, setSelectedPeriod] = useState(new Date())
+  function onChangeDate(date: Date) {
+    setSelectedPeriod(date)
+  }
+
+  console.log(selectedPeriod)
   return (
     <div className="relative rounded-lg shadow bg-gray-800">
       <div className="p-5 gap-3 text-center flex flex-col justify-center items-center">
         <FolderPlus size={52} color="#eee2e2" />
-        <h3 className="mb-5 text-lg font-normal text-gray-400">
+        <h3 className="mb-2 text-lg font-normal text-gray-400">
           Essa ação irá salvar todos os dados abaixo como fechamento do mês
           atual. Caso já exista um relatório salvo para este mês, ele será
           substituído por este.
         </h3>
+        <div>
+          <label
+            htmlFor="selectedPeriod"
+            className="block mb-2 text-md font-medium text-white"
+          >
+            Selecione um período para salvar:
+          </label>
+          <DatePicker
+            selected={selectedPeriod}
+            onChange={(date: Date) => onChangeDate(date)}
+            dateFormat="MM/yyyy"
+            showMonthYearPicker
+            className="mb-5 border text-sm rounded-lg w-full p-2.5 bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
         <div className="flex gap-3">
           <Button
             onClick={onCancel}
@@ -41,7 +63,19 @@ function ConfirmSaveReportModal({
             data-modal-hide="popup-modal"
             type="submit"
             variant="confirm"
-            onClick={() => onSubmit(initialData)}
+            onClick={() => {
+              const formattedDate = new Date(selectedPeriod).toLocaleDateString(
+                'pt-BR',
+                {
+                  month: '2-digit',
+                  year: 'numeric',
+                },
+              )
+              onSubmit({
+                data: initialData,
+                period: formattedDate,
+              })
+            }}
           >
             Confirmar
           </Button>
