c7d50aa7eddb2d101dc4221016b8a423bc7050e9
Matheus Cunha <devmatheusgr@gmail.com>
2024-12-03 12:43:18 +0000

    Update utils.ts

--- src/app/(app)/finance/cards/CardToInvestments/utils.ts
+++ src/app/(app)/finance/cards/CardToInvestments/utils.ts
@@ -123,7 +123,7 @@ export const calculateInvestmentData = (
   const totalAppreciationValue = investimentsData?.ppl || 0
   const investedValue = allPiesData?.result?.priceAvgInvestedValue || 0
   const dividends = (allPiesData?.dividendDetails?.gained || 0) + 0.37
-  const totalJuros = 73.37
+  const totalJuros = 74.47
 
   const appreciationPercentage = (totalAppreciationValue / investedValue) * 100
   const totalInvestedAndGains = totalAppreciationValue + dividends + totalJuros
