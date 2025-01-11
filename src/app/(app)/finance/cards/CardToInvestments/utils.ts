0fea0ac6f0407d1fe5afbacc5b0e6d6f7a6d8494
Matheus Cunha <devmatheusgr@gmail.com>
2025-01-11 15:17:21 +0000

    updated totalJuros

--- src/app/(app)/finance/cards/CardToInvestments/utils.ts
+++ src/app/(app)/finance/cards/CardToInvestments/utils.ts
@@ -117,7 +117,7 @@ export const calculateInvestmentData = (
   const totalAppreciationValue = investimentsData?.ppl || 0
   const investedValue = allPiesData?.result?.priceAvgInvestedValue || 0
   const dividends = (allPiesData?.dividendDetails?.gained || 0) + 0.37
-  const totalJuros = 76.68
+  const totalJuros = 117.43
 
   const appreciationPercentage = (totalAppreciationValue / investedValue) * 100
   const totalInvestedAndGains = totalAppreciationValue + dividends + totalJuros
