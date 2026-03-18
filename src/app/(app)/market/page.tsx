b1a0fce852d1df1a44f25de92843df0911aa7534
Matheus Cunha <devmatheusgr@gmail.com>
2026-03-18 18:16:26 +0000

    add category

--- src/app/(app)/market/page.tsx
+++ src/app/(app)/market/page.tsx
@@ -46,7 +46,8 @@ const MARKET_CATEGORIES = [
   'Hortofrutícolas',
   'Bebidas',
   'Padaria',
-  'Frutaria e Legumes'
+  'Frutaria e Legumes',
+  'Suplementos e Saúde'
 ]
 
 export interface ShoppingItemType {
